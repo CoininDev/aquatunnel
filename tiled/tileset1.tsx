a067e041293caad891c1f02480f2d2bb34cfef8b
CoininDev <gabr.batz@gmail.com>
2025-05-05 11:58:51 -0300

    tilemap working

--- tiled/tileset1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset1" tilewidth="8" tileheight="8" tilecount="2208" columns="46">
- <image source="../assets/dungeon_tiles.png" trans="ff00ff" width="368" height="384"/>
-</tileset>
