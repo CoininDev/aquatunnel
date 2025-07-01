2925653d6c9f0750301ee88a6aab10110fabcd27
Renato Batista <renato.bapxtista@gmail.com>
2025-07-01 08:04:58 -0300

    change harpoon weapon logic and adopt commandbuffer oriented moddifications

--- src/entitites.rs
+++ src/entitites.rs
@@ -29,11 +29,11 @@ pub fn populate(world: &mut World) {
         Player { speed: 100. },
         Body::new(Vec2::new(0.32 / 2., 0.32 / 2.), true),
         WeaponHolder {
-            // weapon: Some(Box::new(Harpoon::default())),
-            weapon: Some(Box::new(DebugGun {
-                active: false,
-                cooldown: 0.,
-            })),
+            weapon: Some(Box::new(Harpoon::default())),
+            // weapon: Some(Box::new(DebugGun {
+            //     active: false,
+            //     cooldown: 0.1,
+            // })),
         },
     ));
 
